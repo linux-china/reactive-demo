39937a51a9d95e91042b57c329cf8ea8e9b74c52
linux_china <linux_china@hotmail.com>
2017-12-28 10:22:39 +0800

    httpbin reponse

---
+++ src/main/java/org/mvnsearch/spring/reactor/HttpBinResponse.java
@@ -0,0 +1,19 @@
+package org.mvnsearch.spring.reactor;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+import java.util.Map;
+
+/**
+ * httpbin reponse
+ *
+ * @author linux_china
+ */
+@Data
+public class HttpBinResponse {
+    private String uuid;
+    @JsonProperty(value = "origin")
+    private String ip;
+    private Map<String, String> headers;
+}
