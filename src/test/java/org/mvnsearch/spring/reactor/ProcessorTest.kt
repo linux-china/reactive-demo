5dc1bfc5909dd0cfaca4b9a6c6742781e11ba6df
linux_china <libing.chen@gmail.com>
2018-11-08 16:38:12 -0800

    Processor test

---
+++ src/test/java/org/mvnsearch/spring/reactor/ProcessorTest.kt
@@ -0,0 +1,22 @@
+package org.mvnsearch.spring.reactor
+
+import org.junit.Test
+import reactor.core.publisher.DirectProcessor
+
+/**
+ * Processor test
+ *
+ * @author linux_china
+ */
+class ProcessorTest {
+
+    @Test
+    fun testSpike() {
+        val processor = DirectProcessor.create<Int>()
+        processor.subscribe {
+            println(it)
+        }
+        processor.onNext(1);
+        processor.onComplete()
+    }
+}
