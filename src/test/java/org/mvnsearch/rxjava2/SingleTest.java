59bf4ca1230b81de7d8c68498791da61c5b29068
linux_china <libing.chen@gmail.com>
2019-01-02 09:29:50 -0800

    Single Test

---
+++ src/test/java/org/mvnsearch/rxjava2/SingleTest.java
@@ -0,0 +1,26 @@
+package org.mvnsearch.rxjava2;
+
+import io.reactivex.Maybe;
+import io.reactivex.Single;
+import org.junit.Test;
+
+/**
+ * Single Test
+ *
+ * @author linux_china
+ */
+@SuppressWarnings("ResultOfMethodCallIgnored")
+public class SingleTest {
+
+    @Test
+    public void testSingle() {
+        Single.just(1).subscribe(System.out::println);
+    }
+
+    @Test
+    public void testMaybe() {
+        Single<Boolean> single = Maybe.just(null).isEmpty();
+        Maybe.just(2).subscribe(System.out::println);
+    }
+
+}
