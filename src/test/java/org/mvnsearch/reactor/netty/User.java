bff1f05327dcd680c628777854f2fdcf864c5553
linux_china <libing.chen@gmail.com>
2019-01-02 09:59:45 -0800

    user

---
+++ src/test/java/org/mvnsearch/reactor/netty/User.java
@@ -0,0 +1,36 @@
+package org.mvnsearch.reactor.netty;
+
+/**
+ * user
+ *
+ * @author linux_china
+ */
+public class User {
+    private Integer id;
+    private String nick;
+
+    public User() {
+    }
+
+
+    public User(Integer id, String nick) {
+        this.id = id;
+        this.nick = nick;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNick() {
+        return nick;
+    }
+
+    public void setNick(String nick) {
+        this.nick = nick;
+    }
+}
