6e3ccc7186b715d7444c469602c9c7921609ae6c
linux_china <libing.chen@gmail.com>
2019-01-08 19:08:25 -0800

    add test method

--- src/test/java/org/mvnsearch/akka/AkkaStreamTest.java
+++ src/test/java/org/mvnsearch/akka/AkkaStreamTest.java
@@ -1,11 +1,18 @@
 package org.mvnsearch.akka;
 
+import akka.Done;
 import akka.NotUsed;
 import akka.actor.ActorSystem;
 import akka.stream.ActorMaterializer;
 import akka.stream.Materializer;
+import akka.stream.javadsl.Flow;
+import akka.stream.javadsl.Sink;
 import akka.stream.javadsl.Source;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import java.util.concurrent.CompletionStage;
 
 /**
  * Akka stream test
@@ -13,11 +20,22 @@ import org.junit.Test;
  * @author linux_china
  */
 public class AkkaStreamTest {
+    private static ActorSystem system;
+    private static Materializer materializer;
+
+    @BeforeClass
+    public static void setUp() {
+        system = ActorSystem.create("QuickStart");
+        materializer = ActorMaterializer.create(system);
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        system.terminate();
+    }
+
     @Test
     public void testDemo() throws Exception {
-        final ActorSystem system = ActorSystem.create("QuickStart");
-        final Materializer materializer = ActorMaterializer.create(system);
-
         final Source<Integer, NotUsed> source = Source.range(1, 10);
         source.map(t -> {
             return t + 1;
@@ -31,11 +49,33 @@ public class AkkaStreamTest {
     @Test
     public void testSingle() throws Exception {
         Source<String, NotUsed> single = Source.single("A");
-        final ActorSystem system = ActorSystem.create("QuickStart");
-        final Materializer materializer = ActorMaterializer.create(system);
         single.runForeach(t -> {
             System.out.println(t);
         }, materializer);
         Thread.sleep(1000);
+    }
+
+    @Test
+    public void testSink() throws Exception {
+        Sink<String, CompletionStage<Done>> print = Sink.foreach(t -> {
+            System.out.println(t);
+        });
+        Source<String, NotUsed> single = Source.single("A");
+        single.runWith(print, materializer);
+        Thread.sleep(1000);
+    }
+
+    @Test
+    public void testFlow() throws Exception {
+        Flow<String, String, NotUsed> flowPrototype = Flow.create();
+        Flow<String, String, NotUsed> flow = flowPrototype.map(t -> {
+            return t + ":";
+        });
+        Source.single("A").via(flow).runForeach(t -> {
+            System.out.println(t);
+        }, materializer);
+        Thread.sleep(1000);
+
+
     }
 }
