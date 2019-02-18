908d0e8e956750d1a1d667ab61aab5f10eaf727d
linux_china <libing.chen@gmail.com>
2019-02-18 14:29:19 -0800

    completable future test

---
+++ src/test/java/org/mvnsearch/future/CompletableFutureTest.java
@@ -0,0 +1,25 @@
+package org.mvnsearch.future;
+
+import org.junit.Test;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * completable future test   https://learning.oreilly.com/library/view/java-high-performance-apps/9781789130515/ch05s04.html
+ *
+ * @author linux_china
+ */
+public class CompletableFutureTest {
+    ExecutorService es = Executors.newFixedThreadPool(10);
+
+    @Test
+    public void testCompletableFuture() throws Exception {
+        CompletableFuture<String> future = CompletableFuture.supplyAsync(() -> "jackie", es);
+        future.whenCompleteAsync((s, throwable) -> {
+            System.out.println(s);
+        });
+        Thread.sleep(1000);
+    }
+}
