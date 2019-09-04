8ee58ff7ab2a059bace17ca29c8dba120e864e3c
linux_china <libing.chen@gmail.com>
2019-09-04 10:33:33 +0800

    pool adjusted

--- src/test/java/org/mvnsearch/reactor/ReactorPoolTest.java
+++ src/test/java/org/mvnsearch/reactor/ReactorPoolTest.java
@@ -4,7 +4,10 @@ import org.junit.Test;
 import reactor.core.publisher.Mono;
 import reactor.pool.Pool;
 import reactor.pool.PoolBuilder;
-import reactor.test.publisher.PublisherProbe;
+import reactor.pool.PoolConfig;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Function;
 
 /**
  * reactor pool test
@@ -14,14 +17,31 @@ import reactor.test.publisher.PublisherProbe;
 public class ReactorPoolTest {
 
     @Test
-    public void testPool() throws Exception{
-        PublisherProbe<Integer> probe = PublisherProbe.of(Mono.fromCallable(() -> {
-            return 1;
-        }));
-        Pool<Integer> pool = PoolBuilder.from(probe.mono()).build();
-        pool.withPoolable(resource->{
-            return Mono.just(resource+1);
+    public void testPool() throws Exception {
+
+        AtomicInteger newCount = new AtomicInteger();
+        PoolBuilder<Integer, PoolConfig<Integer>> builder = PoolBuilder
+                //default maxUse is 5, but this test relies on it being 2
+                .from(Mono.defer(() -> Mono.just(newCount.getAndIncrement())))
+                .sizeBetween(2, 3);
+        Pool<Integer> pool = ReactorPoolTest.<Integer>simplePoolFifo().apply(builder);
+        pool.withPoolable(resource -> {
+            return Mono.just(resource + 1);
         }).subscribe(System.out::println);
         Thread.sleep(1000);
+    }
+
+    static final <T> Function<PoolBuilder<T, ?>, Pool<T>> simplePoolFifo() {
+        return new Function<PoolBuilder<T, ?>, Pool<T>>() {
+            @Override
+            public Pool<T> apply(PoolBuilder<T, ?> builder) {
+                return (Pool<T>) builder.fifo();
+            }
+
+            @Override
+            public String toString() {
+                return "simplePool FIFO";
+            }
+        };
     }
 }
