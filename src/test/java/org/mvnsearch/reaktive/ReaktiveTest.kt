d3cedab26656766b6914376fd96d4f0ae40a0a94
linux_china <libing.chen@gmail.com>
2019-09-13 14:00:23 -0700

    Add test flow

--- src/test/java/org/mvnsearch/reaktive/ReaktiveTest.kt
+++ src/test/java/org/mvnsearch/reaktive/ReaktiveTest.kt
@@ -1,5 +1,7 @@
 package org.mvnsearch.reaktive
 
+import com.badoo.reaktive.observable.observableOf
+import com.badoo.reaktive.observable.subscribe
 import com.badoo.reaktive.single.singleOf
 import com.badoo.reaktive.single.subscribe
 import org.junit.Test
@@ -15,6 +17,15 @@ class ReaktiveTest {
     fun testSpike() {
         val first = singleOf("First")
         first.subscribe {
+            println(it)
+        }
+        Thread.sleep(1000)
+    }
+
+    @Test
+    fun testFlow() {
+        val flow = observableOf("First", "Second")
+        flow.subscribe {
             println(it)
         }
         Thread.sleep(1000)
