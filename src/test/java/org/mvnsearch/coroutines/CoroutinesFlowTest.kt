cf518ea238120b82c3ce3d008aba845f1d324dd6
linux_china <libing.chen@gmail.com>
2020-05-08 17:25:04 -0700

    add testStateFlow

--- src/test/java/org/mvnsearch/coroutines/CoroutinesFlowTest.kt
+++ src/test/java/org/mvnsearch/coroutines/CoroutinesFlowTest.kt
@@ -1,9 +1,7 @@
 package org.mvnsearch.coroutines
 
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.flowOf
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.*
 import org.junit.Test
 
 /**
@@ -18,5 +16,36 @@ class CoroutinesFlowTest {
         val flowA = flowOf(1, 2, 3)
             .map { it + 1 } // Will be executed in ctxA
             .collect { println(it) }
+    }
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun testStateFlow() = runBlocking {
+        val counterModel = CounterModel()
+        launch {
+            for (k in 1..3) {
+                delay(100)
+                counterModel.setName("name: $k")
+            }
+        }
+        launch {
+            counterModel.counter.collect { value -> println("New value: $value") }
+        }
+        launch {
+            counterModel.counter.onEach {
+                println("New2 value: $it")
+            }.launchIn(GlobalScope)
+        }
+        delay(1000)
+    }
+}
+
+@ExperimentalCoroutinesApi
+class CounterModel {
+    private val _counter = MutableStateFlow("") // private mutable state flow
+    val counter: StateFlow<String> get() = _counter // publicly exposed as read-only state flow
+
+    fun setName(name: String) {
+        _counter.value = name
     }
 }
