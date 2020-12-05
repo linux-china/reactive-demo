814f8befeb99d8f59311df82c70ea1b5b382b374
linux_china <libing.chen@gmail.com>
2020-12-04 22:32:52 -0800

    adjusted to rsocket 1.1

--- src/test/java/org/mvnsearch/rsocket/requester/RSocketLocalTest.java
+++ src/test/java/org/mvnsearch/rsocket/requester/RSocketLocalTest.java
@@ -1,8 +1,10 @@
 package org.mvnsearch.rsocket.requester;
 
 import io.rsocket.*;
+import io.rsocket.core.RSocketConnector;
+import io.rsocket.core.RSocketServer;
+import io.rsocket.transport.local.LocalClientTransport;
 import io.rsocket.transport.local.LocalServerTransport;
-import io.rsocket.uri.UriTransportRegistry;
 import io.rsocket.util.DefaultPayload;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -21,11 +23,11 @@ public class RSocketLocalTest {
     @BeforeClass
     public static void setUp() {
         //create a local test server
-        localServer = RSocketFactory.receive()
+        localServer = RSocketServer.create()
                 .acceptor(new SocketAcceptor() {
                     @Override
                     public Mono<RSocket> accept(ConnectionSetupPayload setup, RSocket sendingSocket) {
-                        return Mono.just(new AbstractRSocket() {
+                        return Mono.just(new RSocket() {
                             @Override
                             public Mono<Payload> requestResponse(Payload payload) {
                                 return Mono.just(payload);
@@ -33,16 +35,13 @@ public class RSocketLocalTest {
                         });
                     }
                 })
-                .transport(LocalServerTransport.create("test-local-server"))
-                .start()
+                .bind(LocalServerTransport.create("test-local-server"))
                 .block();
         //rsocket
-        rSocket =
-                RSocketFactory.connect()
-                        .transport(UriTransportRegistry.clientForUri("local:test-local-server"))
-                        .start()
-                        .onTerminateDetach()
-                        .block();
+        rSocket = RSocketConnector.create()
+                .connect(LocalClientTransport.create("local:test-local-server"))
+                .onTerminateDetach()
+                .block();
     }
 
     @AfterClass
