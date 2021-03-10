4db39940115a7a73f1760bf8eaf9834bcaae0f61
linux_china <libing.chen@gmail.com>
2021-03-10 12:34:00 -0800

    Add test methods

--- src/test/java/org/mvnsearch/mutiny/SmallRyeMutinyTest.java
+++ src/test/java/org/mvnsearch/mutiny/SmallRyeMutinyTest.java
@@ -1,7 +1,10 @@
 package org.mvnsearch.mutiny;
 
 import io.smallrye.mutiny.Multi;
+import io.smallrye.mutiny.Uni;
+import io.smallrye.mutiny.converters.uni.UniReactorConverters;
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Mono;
 
 /**
  * SmallRye Mutiny test
@@ -11,10 +14,24 @@ import org.junit.jupiter.api.Test;
 public class SmallRyeMutinyTest {
 
     @Test
-    public void testOperation() {
+    public void testUniOperation() {
+        Uni.createFrom().item("Hello")
+                .onItem().transform(s -> s.toUpperCase() + " ")
+                .subscribe().with(System.out::print);
+    }
+
+    @Test
+    public void testMultiOperation() {
         Multi.createFrom().items("hello", "world")
                 .onItem().transform(s -> s.toUpperCase() + " ")
                 .onCompletion().continueWith("!")
                 .subscribe().with(System.out::print);
+    }
+
+    @Test
+    public void testUniMono() {
+        Uni<String> uni = Uni.createFrom().item("Hello");
+        Mono<String> mono = UniReactorConverters.<String>toMono().apply(uni);
+        System.out.println(mono.block());
     }
 }
